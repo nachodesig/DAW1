c78666699dfb7f366822e57a225af85dda262b07
nachodesig <doritaaarubiii@gmail.com>
2025-10-27 13:04:26 +0100

    gt

--- ExplorandoEclipse/src/Entornos/dd.java
+++ ExplorandoEclipse/src/Entornos/dd.java
@@ -10,13 +10,16 @@ import java.awt.Font;
 import java.awt.Color;
 import java.awt.SystemColor;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JRadioButton;
+import javax.swing.UIManager;
 
 public class dd {
 
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
-	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
@@ -49,7 +52,7 @@ public class dd {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(SystemColor.controlHighlight);
+		frame.getContentPane().setBackground(new Color(227, 227, 227));
 		frame.setBounds(100, 100, 619, 570);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -62,73 +65,85 @@ public class dd {
 		JLabel lblNewLabel_1 = new JLabel("Nombre:");
 		lblNewLabel_1.setForeground(SystemColor.windowText);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1.setBounds(77, 107, 92, 14);
+		lblNewLabel_1.setBounds(110, 106, 92, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Contraseña:");
 		lblNewLabel_1_1.setForeground(SystemColor.windowText);
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1_1.setBounds(77, 158, 92, 14);
+		lblNewLabel_1_1.setBounds(110, 157, 92, 14);
 		frame.getContentPane().add(lblNewLabel_1_1);
 		
 		JLabel lblNewLabel_1_2 = new JLabel("Idioma:");
 		lblNewLabel_1_2.setForeground(SystemColor.windowText);
 		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1_2.setBounds(77, 210, 92, 14);
+		lblNewLabel_1_2.setBounds(110, 209, 92, 14);
 		frame.getContentPane().add(lblNewLabel_1_2);
 		
 		JLabel lblNewLabel_1_3 = new JLabel("Correo electrónico");
 		lblNewLabel_1_3.setForeground(SystemColor.windowText);
 		lblNewLabel_1_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1_3.setBounds(77, 255, 144, 14);
+		lblNewLabel_1_3.setBounds(110, 254, 144, 14);
 		frame.getContentPane().add(lblNewLabel_1_3);
 		
 		JLabel lblNewLabel_1_4 = new JLabel("Comunicación:");
 		lblNewLabel_1_4.setForeground(SystemColor.windowText);
 		lblNewLabel_1_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1_4.setBounds(77, 343, 128, 14);
+		lblNewLabel_1_4.setBounds(110, 342, 128, 14);
 		frame.getContentPane().add(lblNewLabel_1_4);
 		
 		JLabel lblNewLabel_1_5 = new JLabel("Teléfono");
 		lblNewLabel_1_5.setForeground(SystemColor.windowText);
 		lblNewLabel_1_5.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1_5.setBounds(77, 303, 92, 14);
+		lblNewLabel_1_5.setBounds(110, 302, 92, 14);
 		frame.getContentPane().add(lblNewLabel_1_5);
 		
 		JLabel lblNewLabel_1_6 = new JLabel("Observaciones:");
 		lblNewLabel_1_6.setForeground(SystemColor.windowText);
 		lblNewLabel_1_6.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1_6.setBounds(77, 393, 128, 14);
+		lblNewLabel_1_6.setBounds(110, 422, 128, 14);
 		frame.getContentPane().add(lblNewLabel_1_6);
 		
 		textField = new JTextField();
-		textField.setBounds(285, 106, 207, 20);
+		textField.setBounds(335, 106, 157, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(285, 157, 207, 20);
+		textField_1.setBounds(335, 157, 157, 20);
 		frame.getContentPane().add(textField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(285, 209, 207, 20);
-		frame.getContentPane().add(textField_2);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(285, 254, 207, 20);
+		textField_3.setBounds(335, 254, 157, 20);
 		frame.getContentPane().add(textField_3);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
-		textField_4.setBounds(285, 302, 207, 20);
+		textField_4.setBounds(335, 302, 157, 20);
 		frame.getContentPane().add(textField_4);
 		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
-		textField_5.setBounds(285, 392, 268, 63);
+		textField_5.setBounds(335, 422, 218, 63);
 		frame.getContentPane().add(textField_5);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBackground(new Color(192, 192, 192));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Español"}));
+		comboBox.setToolTipText("");
+		comboBox.setBounds(335, 207, 157, 22);
+		frame.getContentPane().add(comboBox);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Teléfono");
+		rdbtnNewRadioButton.setBackground(UIManager.getColor("Button.light"));
+		rdbtnNewRadioButton.setBounds(335, 342, 109, 23);
+		frame.getContentPane().add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnCorreoElectrnico = new JRadioButton("Correo electrónico");
+		rdbtnCorreoElectrnico.setBackground(UIManager.getColor("Button.light"));
+		rdbtnCorreoElectrnico.setBounds(335, 381, 128, 23);
+		frame.getContentPane().add(rdbtnCorreoElectrnico);
 	}
 }
